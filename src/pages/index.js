befa57c45dcda8f60cfe2574dd61bb34a9e44886
Hyuuh <lilhyuuh@gmail.com>
2022-01-17 09:57:52 -0300

    refactor(Root): Added title from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,28 @@
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
-
+import Head from "next/head";
 function Home() {
   return (
-    <main className="bg-black h-screen overflow-hidden">
-      <NavBar />
-      <div className="absolute inset-x-0 bottom-0">
-        <Hero title="Thanks for using this" />
-        <Hero
-          title="NextJS <3"
-          color="bg-gradient-to-r from-indigo-500 to-purple-500"
-          content="we <3 react"
-        />
-        <Hero
-          title="Start in /src/pages/index.js"
-          color="bg-gradient-to-r from-yellow-400 to-pink-500"
-        />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Home - Next.JS</title>
+      </Head>
+      <main className="bg-black h-screen overflow-hidden">
+        <NavBar />
+        <div className="absolute inset-x-0 bottom-0">
+          <Hero title="Thanks for using this" />
+          <Hero
+            title="NextJS <3"
+            color="bg-gradient-to-r from-indigo-500 to-purple-500"
+            content="we <3 react"
+          />
+          <Hero
+            title="Start in /src/pages/index.js"
+            color="bg-gradient-to-r from-yellow-400 to-pink-500"
+          />
+        </div>
+      </main>
+    </>
   );
 }
 
